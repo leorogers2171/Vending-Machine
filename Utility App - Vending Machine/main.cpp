4350a34c101e68953b65657dd9913e8d5db75f5b
Leo Rogers <leo@icustomweb.com>
2020-01-16 14:52:15 +0000

    Additional Functionality

--- Utility App - Vending Machine/main.cpp
+++ Utility App - Vending Machine/main.cpp
@@ -10,8 +10,16 @@
 #include <iomanip>
 #include <iterator>
 #include <array>
+#include <string>
 #include <vector>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <cstdio>
+#include <stdlib.h>
 using namespace std;
+
+string purchasedItem[] = {};
 
 string snacksChoc[] = {
     "Snickers", "Mars", "Twix", "Bounty", "Maltesers",
@@ -35,8 +43,11 @@ string bulkCandy[] = {
     "Jelly Babies", "M&Ms", "Oreos", "Kettle Corn", "Nutella Go"
 };
 
+double categoryPrices[] = {0.8, 0.8, 0.8, 1.3, 1.5};
+
 int menuCounter = 0;
-int balance = 0;
+double balance = 0;
+bool exitVending = false;
 
 
 void DisplayMenu(string listCategory[], int arraySize, string categoryName, double categoryPrice)
@@ -125,35 +136,178 @@ void Instructions()
     cout << "---- Instructions ----" << endl;
     cout << "Use 'M' to view menu" << endl;
     cout << "Use 'B' to view balance" << endl;
+    cout << "Use 'T' to top up balance" << endl;
+    cout << "Use 'E' to exit" << endl;
 }
 
 void DisplayBalance()
 {
-    cout << "Your balance: £ " << balance << endl;
+    cout << endl << "Your balance: £" << balance << endl << endl;
 }
 
+void getNewBalance()
+{
+    cout << endl << "Choose how much to add (£)" << endl;
+    cin >> balance;
+    cout << endl;
+}
 
-bool outputOptions(string userInput)
+float FloatOnePointRounded(float value)
+{
+        return ((float)((int)(value * 10))) / 10;
+}
+
+bool checkOptions(string userInput)
+{
+    transform(userInput.begin(), userInput.end(),userInput.begin(), ::toupper);
+    
+    if (userInput == "M")
+    {
+        return true;
+    }
+    else if (userInput == "B")
+    {
+        return true;
+    }
+    else if (userInput == "E")
+    {
+        return true;
+    }
+    else if (userInput == "T")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void outputOptions(string userInput)
 {
     transform(userInput.begin(), userInput.end(),userInput.begin(), ::toupper);
     
     if(userInput == "M")
     {
         DisplayMenuFull();
-        return true;
     }
     else if(userInput == "B")
     {
         DisplayBalance();
-        return true;
     }
-    return false;
+    else if (userInput == "T")
+    {
+        getNewBalance();
+    }
+    else if(userInput == "E")
+    {
+    }
+}
+
+bool isNumber(string s)
+{
+    for (int i = 0; i < s.length(); i++)
+        if (isdigit(s[i]) == false)
+            return false;
+  
+    return true;
+}
+
+void getBalance()
+{
+    if (balance == 0)
+    {
+        cout << endl << "Please enter your balance (£): ";
+        cin >> balance;
+        cout << endl;
+    }
+}
+
+double getItemPrice(int itemNumber)
+{
+    int sizeOfSnacksChock = sizeof(snacksChoc)/sizeof(snacksChoc[0]),
+    sizeOfSnacksChrisps = sizeof(snacksCrisps)/sizeof(snacksCrisps[0]),
+    sizeOfDrinksCans = sizeof(drinksCans)/sizeof(drinksCans[0]),
+    sizeOfDrinksBottle = sizeof(drinksBottle)/sizeof(drinksBottle[0]),
+    sizeOfBulkCandy = sizeof(bulkCandy)/sizeof(bulkCandy[0]);
+    
+    if (itemNumber >= 0 && itemNumber < sizeOfSnacksChock)
+    {
+        //Price For Chocolate Bars
+        return categoryPrices[0];
+    }
+    
+    else if(itemNumber >= sizeOfSnacksChock && (itemNumber < sizeOfSnacksChock + sizeOfSnacksChrisps))
+    {
+        //Price For Crisps
+        return categoryPrices[1];
+    }
+    
+    else if (itemNumber >= sizeOfSnacksChock + sizeOfSnacksChrisps && (itemNumber < sizeOfSnacksChock + sizeOfSnacksChrisps + sizeOfDrinksCans))
+    {
+        //Price For Drinks Cans
+        return categoryPrices[2];
+    }
+    
+    else if (itemNumber >= sizeOfSnacksChock + sizeOfSnacksChrisps + sizeOfDrinksCans && (itemNumber < sizeOfSnacksChock + sizeOfSnacksChrisps + sizeOfDrinksCans + sizeOfDrinksBottle))
+    {
+        //Price For Drinks Bottles
+        return categoryPrices[3];
+    }
+    
+    else if (itemNumber >= sizeOfSnacksChock + sizeOfSnacksChrisps + sizeOfDrinksCans + sizeOfDrinksBottle && (itemNumber < sizeOfSnacksChock + sizeOfSnacksChrisps + sizeOfDrinksCans + sizeOfDrinksBottle + sizeOfBulkCandy))
+    {
+        //Price For Bulk Candy
+        return categoryPrices[4];
+    }
+
+    else
+    {
+        return 0;
+    }
+}
+
+
+void selectedItem(int itemNumber)
+{
+    double tempBalance = (balance - (getItemPrice(itemNumber-1)));
+    
+    if (tempBalance <= 0)
+    {
+        cout << "You have run out of funds!" << endl;
+        cout << "You need an extra £" << setprecision(2) << stod((to_string(tempBalance).erase(0,1))) << endl << endl;
+    }
+    else
+    {
+        balance = balance - (getItemPrice(itemNumber-1));
+        
+        cout << combineArrays()[itemNumber-1] << " has been dispensed!" << endl;
+        //cout << "You have chosen: " + combineArrays()[itemNumber-1] << endl;
+        cout << "The price is: £" << setprecision(2) <<  getItemPrice(itemNumber-1) << endl;
+        cout << "Your change is: £" << balance << endl << endl;
+    }
+}
+
+
+bool CheckBalance(int itemNumber)
+{
+    int potentialBalance = balance - getItemPrice(itemNumber);
+    if (potentialBalance < 0) {
+        return false;
+    }
+    else {return true;}
+}
+
+void suggestItem(int itemNumber)
+{
+    
 }
 
 
 int main()
 {
-    int itemUserSelects = 0; double userBalance = 0;
+    string itemUserSelects, addMoreFunds;
+    bool balanceFirstSet = false, itemSelected = false;
     DisplayMenuFull();
     cout << "--------------------------------------" << endl;
     Instructions();
@@ -161,27 +315,66 @@ int main()
     
     while(true)
     {
-        cout << "Which item would you like to purchase? (1 - " << menuCounter << ")" << endl;
+        cout << "Choose item to purchase? (1 - " << menuCounter << ")" << endl << "Or Option (M, B, T, E)" << endl;
         cin >> itemUserSelects;
         cin.clear();
         cin.ignore(256, '\n');
         
-        if(itemUserSelects < 1 || itemUserSelects > menuCounter && outputOptions(itemUserSelects)) == false)
+        if(itemUserSelects == "E" || itemUserSelects == "e")
         {
+            cout << endl << endl << "Thank you for using our machine" << endl;
             break;
         }
-    }
-    
-    cout << "What is your balance?" << endl << "£ ";
-    cin >> userBalance;
-    
-    
-    while (cin.fail())
-    {
-        cout << "Invalid input" << endl << endl;
-        cout << "What is your balance?" << endl << "£ ";
-        cin.clear();
-        cin.ignore(256, '\n');
-        cin >> userBalance;
+        
+//        if(itemSelected == true)
+//        {
+//            suggestItem(stoi(itemUserSelects));
+//        }
+        
+        if(isNumber(itemUserSelects))
+        {
+            if(CheckBalance(stoi(itemUserSelects)) && balanceFirstSet == false)
+            {
+                //Balance hasn't been set initially
+                balanceFirstSet = true;
+                getBalance();
+            }
+            else if (!CheckBalance(stoi(itemUserSelects)) && balanceFirstSet == true)
+            {
+                //Balance has already been set and run out
+                cout << "Your balance has run out!" << endl <<
+                "Do you want to add more funds (y/n)" << endl;
+                
+                cin.clear();
+                cin.ignore(256, '\n');
+                cin >> addMoreFunds;
+                
+                if (addMoreFunds == "y" || addMoreFunds == "Y") {
+                    getBalance();
+                }
+                else {break;}
+            }
+        }
+        
+        if(isNumber(itemUserSelects) && (stoi(itemUserSelects) < 1 || stoi(itemUserSelects) > menuCounter))
+        {
+            cout << endl << "Choose a number in the valid range" << endl;
+        }
+        
+        else if(checkOptions(itemUserSelects))
+        {
+            outputOptions(itemUserSelects);
+        }
+        
+        else if(!checkOptions(itemUserSelects) && !isNumber(itemUserSelects))
+        {
+            cout << "Please choose a letter in the valid range" << endl;
+        }
+        
+        else
+        {
+            selectedItem(stoi(itemUserSelects));
+            itemSelected = true;
+        }
     }
 }
